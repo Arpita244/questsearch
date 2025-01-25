59793a968201ed3836fffabb030abfd8ce35933f
arpita244 <arpitatiwari8756@gmail.com>
2025-01-26 00:50:16 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    if (query || page > 1) {
+    if (query || page >= 1) {
       handleSearch();
     }
   }, [query, page]); // Trigger search whenever query or page changes
